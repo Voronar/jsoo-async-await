c8bda6cd4ecea1476b62d3ad589e5d507b2c2864
Kirill A. Khalitov <voronar@gmail.com>
2019-08-02 20:21:14 +0300

    initial

---
+++ promise.mli
@@ -0,0 +1,65 @@
+type ('a, 'b) t
+
+type 'a resolve = 'a -> unit
+
+type 'a reject = 'a -> unit
+
+val is_supported : unit -> bool
+
+val make : ('a resolve -> 'b reject -> unit) -> ('a, 'b) t
+
+val resolve : 'a -> ('a, 'b) t
+
+val reject : 'b -> ('a, 'b) t
+
+val then_bind :
+  on_fulfilled:('a -> ('c ,'b) t) ->
+  ?on_rejected:('b -> ('c, 'b) t) ->
+  ('a, 'b) t ->
+  ('c, 'b) t
+
+val then_map :
+  on_fulfilled:('a -> 'c) ->
+  ?on_rejected:('b -> 'c) ->
+  ('a, 'b) t ->
+  ('c, 'b) t
+
+val catch_bind :
+  on_rejected:('b -> ('a, 'b) t) ->
+  ('a, 'b) t ->
+  ('a, 'b) t
+
+val catch_map :
+  on_rejected:('b -> 'a) ->
+  ('a, 'b) t ->
+  ('a, 'b) t
+
+val then_final :
+  on_fulfilled:('a -> unit) ->
+  on_rejected:('b -> unit) ->
+  ('a, 'b) t ->
+  unit
+
+val catch_final :
+  ('b -> unit) ->
+  ('a, 'b) t ->
+  unit
+
+val all : (('a, 'b) t) array -> ('a array, 'b) t
+
+val race : (('a, 'b) t) array -> ('a, 'b) t
+
+module Infix : sig
+  val (>>=) : ('a, 'b) t -> ('a -> ('c ,'b) t) -> ('c, 'b) t
+  val (>|=) : ('a, 'b) t -> ('a -> 'c) -> ('c, 'b) t
+
+  val (>>~) : ('a, 'b) t -> ('b -> ('a, 'b) t) -> ('a, 'b) t
+  val (>|~) : ('a, 'b) t -> ('b -> 'a) -> ('a, 'b) t
+
+  val (>||) : ('a, 'b) t -> ('a -> unit) * ('b -> unit) -> unit
+end
+
+module Async : sig
+  val ( let* ): ('a, 'b) t -> ('a -> ('c, 'b) t) -> ('c, 'b) t
+  val ( let+ ): ('a, 'b) t -> ('a -> 'c) -> ('c, 'b) t
+end
